6f9b2a8fdfbe644cac1a94c167a673ea8ae02096
NitinNanda <nitinnanda11@gmail.com>
2020-11-25 01:15:33 -0500

    Creating top Nav bar

--- pizza-project-ui/src/layouts/MainLayout.vue
+++ pizza-project-ui/src/layouts/MainLayout.vue
@@ -2,6 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
+
         <q-btn
           flat
           dense
@@ -11,17 +12,18 @@
           @click="leftDrawerOpen = !leftDrawerOpen"
         />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <q-btn stretch flat label="Menu" />
+        <q-btn stretch flat label="Coupons" />
+        <q-btn stretch flat label="Location" />
+        <q-btn stretch flat label="Tracker" />
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-space/>
+        <q-icon name="shopping_cart" size="md"/>
       </q-toolbar>
     </q-header>
 
     <q-drawer
       v-model="leftDrawerOpen"
-      show-if-above
       bordered
       content-class="bg-grey-1"
     >
@@ -50,48 +52,6 @@
 import EssentialLink from 'components/EssentialLink.vue'
 
 const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
 ]
 
 export default {
